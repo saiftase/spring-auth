5c435d641290140adcf2bce6728e3deeff0d46ce
Saif Tase <saiftase@gmail.com>
2018-04-10 13:25:19 -0400

    Added spring-boot-starter-security dependency, created Role and User entities, updated seed file.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+-- Insert roles
+insert into role (name) values ('ROLE_USER');
+
+-- Insert a couple users
+insert into user (username,enabled,password,role_id) values ('user',true,'password',1);
+insert into user (username,enabled,password,role_id) values ('user2',true,'password',1);
+
 -- Insert tasks
 insert into task (complete,description) values (true,'Code Task entity');
 insert into task (complete,description) values (false,'Discuss users and roles');
